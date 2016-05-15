a443910671e77829eb7e0f9fc8a98114f5568e69
Conchylicultor <etiennefg.pot@gmail.com>
2016-05-15 15:48:18 +0200

    Try collisions

---
+++ Assets/Prefabs/Objects/CollisionPers.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionPers : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "persr") {
+			Debug.Log("Person killed");
+			Debug.Log(other.transform.position);
+			Debug.Log(this.transform.position);
+			Debug.Log(other.tag);
+
+			//Destroy(other.gameObject);
+			//Destroy(gameObject);
+		}
+	}
+}
